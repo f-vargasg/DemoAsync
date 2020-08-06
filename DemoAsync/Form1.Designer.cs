c7bd813863c14eaf7780f28c16ae16df40184675
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2020-08-05 22:15:47 -0600

    Esta es la segunda parte del tutorial mejorado

--- DemoAsync/Form1.Designer.cs
+++ DemoAsync/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // button1
@@ -40,7 +41,6 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Ejecutar Operacion";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
@@ -51,11 +51,19 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Esperando";
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(375, 450);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(401, 53);
+            this.progressBar1.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -71,6 +79,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- DemoAsync/Form1.cs
+++ DemoAsync/Form1.cs
@@ -15,27 +15,62 @@ namespace DemoAsync
         public Form1()
         {
             InitializeComponent();
+            InitMyComponents();
         }
 
-        float OperacionLarga(int parametro)
+        private void InitMyComponents()
         {
-            System.Threading.Thread.Sleep(1000);
-            throw new Exception("Excepcion");
-            return 25 + parametro;
+            // this.button1.Click += ClickSincrono;
+            // Se ejecuta en otro hilo
+            this.button1.Click += ClickAsincrono;
         }
 
-        private async void button1_Click(object sender, EventArgs e)
+        private void ClickSincrono(object sender, EventArgs e)
+        {
+            try
+            {
+
+                UseWaitCursor = true;
+                label1.Text = "Ejecutando Operacion...";
+
+                var progreso = new Progress<int>(pct => progressBar1.Value = pct);
+
+                int par = 50;
+                float result = 0;
+                try
+                {
+                    result = OperacionLarga(par, progreso);
+
+                    label1.Text = "Finalizado " + result.ToString();
+                }
+                catch (Exception ex)
+                {
+                    label1.Text = "Error " + ex.Message;
+                }
+
+
+                UseWaitCursor = false;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show (ex.Message);
+            }
+        }
+
+        private async void ClickAsincrono(object sender, EventArgs e)
         {
             UseWaitCursor = true;
             label1.Text = "Ejecutando Operacion...";
+
+            var progreso = new Progress<int>(pct => progressBar1.Value = pct);
+
             int par = 50;
             float result = 0;
             try
             {
-                result = await Task.Run<float>(() => { return OperacionLarga(par); });
-                result += await Task.Run<float>(() => { return OperacionLarga(par); });
-                result += await Task.Run<float>(() => { return OperacionLarga(par); });
-                result += await Task.Run<float>(() => { return OperacionLarga(par); });
+                result = await Task.Run<float>(() => { return OperacionLarga(par, progreso); });
+
                 label1.Text = "Finalizado " + result.ToString();
             }
             catch (Exception ex)
@@ -46,5 +81,50 @@ namespace DemoAsync
 
             UseWaitCursor = false;
         }
+
+
+
+        /*
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            UseWaitCursor = true;
+            label1.Text = "Ejecutando Operacion...";
+
+            var progreso = new Progress<int>(pct => progressBar1.Value = pct);
+
+            int par = 50;
+            float result = 0;
+            try
+            {
+                result = await Task.Run<float>(() => { return OperacionLarga(par, progreso); });
+
+                label1.Text = "Finalizado " + result.ToString();
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "Error " + ex.Message;
+            }
+
+
+            UseWaitCursor = false;
+        }
+        */
+
+
+        float OperacionLarga(int parametro, IProgress<int> progreso)
+        {
+            for (int i = 0; i <= 100; i+=10)
+            {
+                System.Threading.Thread.Sleep( 100);
+                if (progreso != null)
+                {
+                    progreso.Report(i);
+                }
+            }
+            
+            return 25 + parametro;
+        }
+
+        
     }
 }
